01af1cd899a80d19da4c655fbd415cfa604cef87
naiknetra <naiknetra5@gmail.com>
2024-11-12 12:05:21 -0500

    updated insex file

--- src/index.ts
+++ src/index.ts
@@ -28,10 +28,62 @@ inquirer
     if (answers.Action == 'View All Employees'){
       const res = await pool.query('SELECT * FROM employee')
         console.table( res.rows)
+        }
+        else 
         
+        
+        if (answers.Action == 'Add Employee'){
+          const employeeDetails = await inquirer.prompt([
+            {
+              type: 'input',
+              name: 'first_name',
+              message: 'Enter the employee\'s first name:',
+            },
+            {
+              type: 'input',
+              name: 'last_name',
+              message: 'Enter the employee\'s last name:',
+            },
+            {
+              type: 'input',
+              name: 'role_id',
+              message: 'Enter the employee\'s role ID:',
+            },
+            {
+              type: 'input',
+              name: 'manager_id',
+              message: 'Enter the employee\'s manager ID (or leave blank if none):',
+            }
+          ]);
+    
+          // Prepare the SQL INSERT statement
+          const { first_name, last_name, role_id, manager_id } = employeeDetails;
+          const query = `
+            INSERT INTO employee (first_name, last_name, role_id, manager_id) 
+            VALUES ($1, $2, $3, $4) RETURNING *;
+          `;
+          
+          // Execute the query
+          const res = await pool.query(query, [first_name, last_name, role_id, manager_id || null]);
+          console.log('Employee added:', res.rows[0]);
+        }
 
-    }
+        else if  (answers.Action == 'View All Roles'){
+          const res = await pool.query('SELECT * FROM role')
+            console.table( res.rows)
+            }
+
+            else if  (answers.Action == 'View All Departments'){
+              const res = await pool.query('SELECT * FROM department')
+                console.table( res.rows)
+            }
+        // Add more else if statements for other actions...
+      });
+
+
+
+        
 
     
     // Use user feedback for... whatever!!
-    })
+    
